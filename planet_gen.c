4ba9f1fc6ffaffd4fbd4f7a1676b2090dab27104
Ben Tatman <ben@tatmans.co.uk>
2013-12-14 11:19:51 +0000

    Sorted indentation

--- planet_gen.c
+++ planet_gen.c
@@ -8,25 +8,25 @@
 #define M 1.989*(pow(10,30))
 
 typedef struct {
-  float radius;
-  float mass;
-  float density;
-  char name[500];
-  float distance;
-  float orbital_period;
+	float radius;
+	float mass;
+	float density;
+	char name[500];
+	float distance;
+	float orbital_period;
 } Moon;
-                 
+								 
 typedef struct {
-  float radius;
-  float mass;
-  float density;
-  char * desc;
-  char * living;
-  char name[500];
-  float solar_distance;
-  float orbital_period;
-  Moon moon[3];
-  int moon_number;
+	float radius;
+	float mass;
+	float density;
+	char * desc;
+	char * living;
+	char name[500];
+	float solar_distance;
+	float orbital_period;
+	Moon moon[3];
+	int moon_number;
 } Planet;
 
 char pre_name[500][500] = {"Plu", "Jup", "So", "Ea", "Ven", "Mer"};
@@ -60,33 +60,33 @@ Moon generate_moon(Planet p) {
 	r.orbital_period = (2*M_PI*sqrt((pow(r.distance*1000, 3))/(G*p.mass)));
 	return r; 
 }
-                 
+								 
 Planet generate_planet(void) {
-  Planet r;
-  r.radius = rand()%7000 + 1000;
-  float masskg = ((rand()%10)+1) * (pow(10, 12));
-  r.density = masskg;
-  /*We do not calculate the SOI here as there are no other objects which could affect the orbit, so it just orbits
-   * the sun no matter the distance.*/
-  r.solar_distance = ((rand()%1000)+1) * (pow(10, rand()%10+5));
-  float area = 4/3*M_PI*(pow(r.radius, 3));
-  
-  r.mass = area * masskg;
-  
-  sprintf(r.name, "%s%s", pre_name[rand()%name_types], suc_name[rand()%name_types]);
-  r.desc = desc[rand()%desc_types];
-  r.living = living[rand()%living_types];
-  
-  r.orbital_period = (2*M_PI*sqrt((pow(r.solar_distance*1000, 3))/(G*M))); // In Seconds
-  
-  r.moon_number = rand()%3;
-  
-  int i;
-  for (i = 0; i < r.moon_number; i++) {
-	  r.moon[i] = generate_moon(r);
+	Planet r;
+	r.radius = rand()%7000 + 1000;
+	float masskg = ((rand()%10)+1) * (pow(10, 12));
+	r.density = masskg;
+	/*We do not calculate the SOI here as there are no other objects which could affect the orbit, so it just orbits
+	 * the sun no matter the distance.*/
+	r.solar_distance = ((rand()%1000)+1) * (pow(10, rand()%10+5));
+	float area = 4/3*M_PI*(pow(r.radius, 3));
+	
+	r.mass = area * masskg;
+	
+	sprintf(r.name, "%s%s", pre_name[rand()%name_types], suc_name[rand()%name_types]);
+	r.desc = desc[rand()%desc_types];
+	r.living = living[rand()%living_types];
+	
+	r.orbital_period = (2*M_PI*sqrt((pow(r.solar_distance*1000, 3))/(G*M))); // In Seconds
+	
+	r.moon_number = rand()%3;
+	
+	int i;
+	for (i = 0; i < r.moon_number; i++) {
+		r.moon[i] = generate_moon(r);
 	}
-  
-  return r;
+	
+	return r;
 }
 
 int print_moon(Moon p, int indent) {
@@ -105,44 +105,44 @@ int print_moon(Moon p, int indent) {
 	return 1;
 }
 int print_planet(Planet r) {
-  printf("%s\n", r.name);
-  int q;
-  for (q = 0; q < strlen(r.name); q++) {
-    printf("-");
-  }
-  printf("\n");
-  printf("%s %s\n", r.desc, r.living);
-  printf("Mass: %gkg\n", r.mass);
+	printf("%s\n", r.name);
+	int q;
+	for (q = 0; q < strlen(r.name); q++) {
+		printf("-");
+	}
+	printf("\n");
+	printf("%s %s\n", r.desc, r.living);
+	printf("Mass: %gkg\n", r.mass);
 	printf("Radius: %gkm\n", r.radius);
 	printf("Density: %gkg/m^3\n", r.density);
-  printf("SOI: %gkm\n", (r.solar_distance)*(pow((r.mass / M), (2/5))));
-  printf("Distance from Sun: %gkm\n", r.solar_distance);
-  
-  printf("Orbital Period: %gs\n", r.orbital_period);
-  
-  printf("Speed: %fkm/s\n", (M_PI*(2*r.solar_distance))/r.orbital_period);
-  
-  printf("Moons: %d\n", r.moon_number);
-  
-  int i;
-  for (i = 0; i < r.moon_number; i++) {
-	  printf("\n");
-	  print_moon(r.moon[i], 1);
+	printf("SOI: %gkm\n", (r.solar_distance)*(pow((r.mass / M), (2/5))));
+	printf("Distance from Sun: %gkm\n", r.solar_distance);
+	
+	printf("Orbital Period: %gs\n", r.orbital_period);
+	
+	printf("Speed: %fkm/s\n", (M_PI*(2*r.solar_distance))/r.orbital_period);
+	
+	printf("Moons: %d\n", r.moon_number);
+	
+	int i;
+	for (i = 0; i < r.moon_number; i++) {
+		printf("\n");
+		print_moon(r.moon[i], 1);
 	 }
-  printf("\n\n");
-  return 1;
+	printf("\n\n");
+	return 1;
 }
 
 int main(int argc, char * argv[]) {
-  srand(time(NULL));
-  if (argc == 2) {
-    int count = atoi(argv[1]);
-    int i;
+	srand(time(NULL));
+	if (argc == 2) {
+		int count = atoi(argv[1]);
+		int i;
 
-    for (i = 0; i < count; i++) {
-      planets[i] = generate_planet();
-      print_planet(planets[i]);
-    }
-  }
-  return 1;
+		for (i = 0; i < count; i++) {
+			planets[i] = generate_planet();
+			print_planet(planets[i]);
+		}
+	}
+	return 1;
 }
